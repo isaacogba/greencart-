043d9f1d2273a1bd114dfe4d971f973a8c8bbc4b
Isaac Ogba <ogbaisaac1@gmail.com>
2026-06-01 04:41:05 +0100

    seting the grid well

--- client/src/page/ProductCategory.jsx
+++ client/src/page/ProductCategory.jsx
@@ -20,7 +20,7 @@ const searchCategory = categories.find((item)=>item.path.toLowerCase() === categ
         </div>
       )}
       {filteredProducts.length > 0 ? (
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 md:gap-8 mt-6'>
             {filteredProducts.map((product)=>(
                 <ProductCard key={product._id} product={product}/>
             ))}
--- client/src/page/ProductDetails.jsx
+++ client/src/page/ProductDetails.jsx
@@ -89,7 +89,7 @@ const ProductDeatails = () => {
                     <p className="text-3xl font-medium">Related Products</p>
                     <div className="w-20 h-0.5 bg-primary rounded-full mt-2"></div>
                 </div>
-                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6 w-full">
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 md:gap-8 lg:grid-cols-5 mt-6 w-full">
                     {relatedProducts.filter((product)=>product.inStock).map((product, index)=>(
                         <ProductCard key={index} product={product}/>
                     ))}
